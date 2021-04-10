f8cf1a5a9dba026ce8fab7f127deca1af5b8fc81
Lili Cohen <lili.cohen.tv@gmail.com>
2021-04-09 17:11:46 -0700

    adding all images

--- client/src/components/Carousel/Carousel.js
+++ client/src/components/Carousel/Carousel.js
@@ -2,12 +2,22 @@ import React, { useState } from 'react';
 import img1 from '../../images/Laura-Carousel/img1.png';
 import img2 from '../../images/Laura-Carousel/img2.png';
 import img3 from '../../images/Laura-Carousel/img3.png';
+import img4 from '../../images/Laura-Carousel/img4.png';
+import img5 from '../../images/Laura-Carousel/img5.png';
+import img6 from '../../images/Laura-Carousel/img6.png';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 import '../../styles/Carousel.css';
 
 const Carousel = () => {
-  const images = [{ img: img1 }, { img: img2 }, { img: img3 }];
+  const images = [
+    { img: img1 },
+    { img: img2 },
+    { img: img3 },
+    { img: img4 },
+    { img: img5 },
+    { img: img6 },
+  ];
   const [currImg, setCurrImg] = useState(0);
   return (
     <div className="carousel">
