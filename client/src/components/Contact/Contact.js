72944dfe8627b7e1aecf602b0c2956b135e7ca3e
Lili Cohen <lili.cohen.tv@gmail.com>
2021-04-11 19:22:37 -0700

    starting form on contact page

--- client/src/components/Contact/Contact.js
+++ client/src/components/Contact/Contact.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import '../../styles/Contact.css';
 
 const Contact = () => {
   return (
     <div>
-      <h1>Hello World, I'm Contact</h1>
+      <br></br>
+      <br></br>
+      <br></br>
+      <h1 id="h1C">Contact</h1>
+      <h3>Send me a message</h3>
+      <div className="background">
+        <form>
+          <input></input>
+          <textarea></textarea>
+        </form>
+      </div>
     </div>
   );
 };
