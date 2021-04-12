c37c830df1331400bae4e44ba2c78b3c675f64cd
Lili Cohen <lili.cohen.tv@gmail.com>
2021-04-11 18:59:10 -0700

    changing nav

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -3,13 +3,9 @@ import '../../styles/Nav.css';
 
 function Nav() {
   // TODO: add state in order for navbar show and hide to work
-  if (
-    window.location.pathname === '/home' ||
-    window.location.pathname === '/'
-  ) {
-    return (
-      <div className="cont">
-        <div></div>
+  return (
+    <div className="cont">
+      <div className="row">
         <ul>
           <li className="item tabs">
             <Link
@@ -35,56 +31,21 @@ function Nav() {
               Portfolio
             </Link>
           </li>
+          <li className="item tabs">
+            <Link
+              id="underline"
+              to="/home"
+              className={
+                window.location.pathname === '/home' ? 'link active' : 'link'
+              }
+            >
+              Home
+            </Link>
+          </li>
         </ul>
       </div>
-    );
-  } else {
-    return (
-      <div className="cont">
-        <div className="row">
-          <ul>
-            <li className="item tabs">
-              <Link
-                id="underline"
-                to="/contact"
-                className={
-                  window.location.pathname === '/contact'
-                    ? 'link active'
-                    : 'link'
-                }
-              >
-                Contact
-              </Link>
-            </li>
-            <li className="item tabs">
-              <Link
-                id="underline"
-                to="/portfolio"
-                className={
-                  window.location.pathname === '/portfolio'
-                    ? 'link active'
-                    : 'link'
-                }
-              >
-                Portfolio
-              </Link>
-            </li>
-            <li className="item tabs">
-              <Link
-                id="underline"
-                to="/home"
-                className={
-                  window.location.pathname === '/home' ? 'link active' : 'link'
-                }
-              >
-                Lili Cohen
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Nav;
